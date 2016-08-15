af09869dcaa6f84c0432691fe5906501abe258b4
caroldomokos <carol.domokos@gmail.com>
2016-08-15 15:37:02 +0200

    Update euler001.clj
    reduced the scope to 2 var. example needs update. corrected mult of mult flaw

--- euler001.clj
+++ euler001.clj
@@ -3,9 +3,9 @@
   [mult rangeend]
   (reduce + (#(range % %2 %3) mult rangeend mult)))
 (defn euler001
-  "Find the sum of all the multiples of [keys] or 5 below [ends]"
-  [ends keys]
-  (reduce + (map sumofmult keys ends)))
+  "Find the sum of all the multiples of key1 and key2 bellow end  
+  [key1 key2 end]
+ (- (+ (sumofmult key1 end) (sumofmult key2 end)) (sumofmult (* key1 key2) end)))
 ; example for the original problem form project euler
 ; Find the sum of all the multiples of 3 or 5 below 1000.
 ; user=> (euler001 [100 100] [3 5])
