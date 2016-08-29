ca2f1a20a1f28347cd3c661bc515f61641d6186d
caroldomokos <carol.domokos@gmail.com>
2016-08-29 14:13:14 +0200

    Update euler051.clj
    created prime generator

--- euler051.clj
+++ euler051.clj
@@ -1,1 +1,14 @@
 ;; placeholder
+(defn middle
+[number]
+(if (integer? (/ number 2))
+  number
+(/ ( - number 1) 2)))
+;; we want to check if a number is divisible with all numbers up to (rounded -floor like) haf of its value
+(defn prime
+  [number]
+  (if (empty?(filter integer? (map #(/ number %) (range 2 number))))
+   number))
+  (defn primegen
+  [start end]
+  (remove #(= nil %) (map prime (range start end))))
