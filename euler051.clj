76687ebf8c6530fab89faacfead80f232dba5957
caroldomokos <carol.domokos@gmail.com>
2016-08-31 12:05:22 +0200

    Update euler051.clj

--- euler051.clj
+++ euler051.clj
@@ -12,3 +12,31 @@
   (defn primegen
   [start end]
   (remove #(= nil %) (map prime (range start end))))
+;;; I need to transform the numbers into vectors
+(defn lastdigit
+  ([number] (lastdigit number 0))
+  ([number lastdigit]
+   (if (integer? (/ number 10))
+     lastdigit
+     (recur (dec number) (inc lastdigit)))))
+(defn vectorize
+  ([number] (vectorize number []))
+  ([number vectorized] 
+    (if (= number 0)
+      vectorized
+(recur (/ (- number (lastdigit number)) 10) (into vectorized (vector (lastdigit number)))))))
+
+(defn duplicateList-bad
+  ([number] (duplicatelist number[])
+  ([number duplicates]
+    (if (empty? number)
+      duplicates
+      (if (some (first number) (rest number))
+          (duplicatelist (rest number) (set (into duplicates (first number))))
+          (duplicatelist (rest number) duplicates)))))
+
+(defn duplicate
+  [numvect position]
+  (if  (some #(= % (nth numvect position))(nthnext numvect (inc position))) (nth numvect position)))
+  
+  
